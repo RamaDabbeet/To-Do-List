b24db58b98f79de45d418cb3201b0c3ce89fd0cc
Rama Dabbeet <ramadabbeet@gmail.com>
2022-06-24 13:36:03 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+// eslint-disable-next-line no-unused-vars
+import style from "./style.css"
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"))
